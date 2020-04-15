af2388f7a0e85c680ca3fa130cc75fa6da896185
john akins <johnakins.dev@gmail.com>
2020-04-15 22:26:52 +0100

    updates server file configuration

--- src/server.js
+++ src/server.js
@@ -31,15 +31,12 @@ const errorHandler = (error) => {
   if (error.syscall !== 'listen') {
     throw error;
   }
-  const address = server.address();
-  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind}requires elevated priviledges.`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(`${bind}is already in use.`);
       process.exit(1);
       break;
     default:
@@ -48,12 +45,6 @@ const errorHandler = (error) => {
 };
 
 server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
-  console.log(address);
-  console.log(`Listening on ${bind}`);
-});
 
 // set server to listen with either production or local port
 server.listen(port);
